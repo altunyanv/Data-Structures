673eb4c4138b28264abb477ca187daacaad2d6bf
altunnyanv <altunnyanv@gmail.com>
2019-09-30 16:48:06 +0400

    Vector naive efficient added

--- linear_ds/vector.hpp
+++ linear_ds/vector.hpp
@@ -3,10 +3,83 @@
 class vector {
 private:
   int * container;
-  int size;
+  int size, capacity;
 public:
-  vector(int * _container, int _size) : size(_size), container(_container) {
+  vector(int * _container, int _size) : size(_size), container(new int[_size]), capacity(_size) {
     for(int i = 0; i < size; ++i)
       container[i] = _container[i];
+  }
+
+  void push_back(int value) {
+    if (size < capacity) {
+      container[size] = value;
+      size++;
+    }
+    else {
+      int nCapacity = 2 * capacity;
+      int * nContainer = new int[nCapacity];
+      for(int i = 0; i < size; i++) {
+        nContainer[i] = container[i];
+      }
+      capacity = nCapacity;
+      int * tmp = container;
+      container = nContainer;
+      delete[] tmp;
+      container[size] = value;
+      size++;
+    }
+  }
+
+  void pop_back() {
+    if (size * 4 > capacity) {
+      size--;
+    }
+    else {
+      int nCapacity = capacity / 2;
+      int * nContainer = new int[nCapacity];
+      for(int i = 0; i < size - 1; i++) {
+        nContainer[i] = container[i];
+      }
+      capacity = nCapacity;
+      int * tmp = container;
+      container = nContainer;
+      delete[] tmp;
+      size--;
+    }
+  }
+};
+
+
+class vectorNaive {
+private:
+  int * container;
+  int size, capacity;
+public:
+  vectorNaive(int * _container, int _size) : size(_size), container(new int[_size]), capacity(_size) {
+    for(int i = 0; i < size; ++i)
+      container[i] = _container[i];
+  }
+
+  void push_back(int value) {
+    int * nContainer = new int[size + 1];
+    for(int i = 0; i < size; i++) {
+      nContainer[i] = container[i];
+    }
+    nContainer[size] = value;
+    size++;
+    int * tmp = container;
+    container = nContainer;
+    delete[] tmp;
+  }
+
+  void pop_back() {
+    int * nContainer = new int[size - 1];
+    for(int i = 0; i < size - 1; i++) {
+      nContainer[i] = container[i];
+    }
+    size--;
+    int * tmp = container;
+    container = nContainer;
+    delete[] tmp;
   }
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+// #include "linear_ds\vector.hpp"
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+  vector<pair<int, int>> vec;
+  vec.push_back(make_pair(1, 1));
+  vec.emplace_back(1, 1);
+
+  return 0;
+  // int a[] = {1, 2};
+  // vector vec(a, 2);
+  // clock_t start = clock();
+  // for(int i = 0; i < 10000000; i++) {
+  //   vec.push_back(i);
+  // }
+  // for(int i = 0; i < 10000000; i++) {
+  //   vec.pop_back();
+  // }
+  // cout << (clock() - start + 0.) / CLOCKS_PER_SEC << endl;
+
+  // vectorNaive vect(a, 2);
+  // clock_t star = clock();
+  // for(int i = 0; i < 10000; i++) {
+  //   vect.push_back(i);
+  // }
+  // for(int i = 0; i < 10000; i++) {
+  //   vect.pop_back();
+  // }
+  // cout << (clock() - star + 0.) / CLOCKS_PER_SEC << endl;
+  
   system("pause");
   return 0;
 }
